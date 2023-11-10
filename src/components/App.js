b3b0674a5a7d56d994364ce9510306eef7d98a54
VICTOR <victor.kipkoech@student.moringaschool.com>
2023-11-10 12:51:59 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        setLoading(false);
+        setDogImage(data.message);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
